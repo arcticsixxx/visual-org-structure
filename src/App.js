e4fe4b9fd5122382296162962556893af85a7228
Лыжин Алексей Вячеславович <aleksey.lyzhin@rt.ru>
2024-12-13 09:21:13 +0500

    (feature): Flow - init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ReactFlow, Background } from "@xyflow/react";
 
-function App() {
+import "@xyflow/react/dist/style.css";
+
+const initialNodes = [
+  { id: "1", position: { x: 0, y: 0 }, data: { label: "Локация" } },
+  { id: "2", position: { x: 0, y: 100 }, data: { label: "Подразделение" } },
+  { id: "3", position: { x: 0, y: 200 }, data: { label: "Отдел" } },
+  { id: "4", position: { x: 0, y: 300 }, data: { label: "Группа" } },
+  { id: "5", position: { x: 0, y: 400 }, data: { label: "Сотрудник" } },
+];
+const initialEdges = [
+  { id: "e1-2", source: "1", target: "2" },
+  { id: "e2-3", source: "2", target: "3" },
+  { id: "e3-4", source: "3", target: "4" },
+  { id: "e4-5", source: "4", target: "5" },
+];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <ReactFlow nodes={initialNodes} edges={initialEdges}>
+        <Background variant="dots" gap={12} size={1} />
+      </ReactFlow>
     </div>
   );
 }
-
-export default App;
